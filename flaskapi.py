09a06a7f6a6497a45b18cbd982a6d9da7f776edc
BillyGareth <billygareth01@gmail.com>
2020-04-16 13:26:36 +0300

    /test message

--- flaskapi.py
+++ flaskapi.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app=Flask('__name__')
-​
-app.route('/')
+app = Flask(__name__)
+
+@app.route('/test')
 def first_api():
     return 'Hello from Gareth'
-​
+
 if __name__ == '__main__':
     app.run()
